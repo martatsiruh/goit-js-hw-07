75c74b5a6bc851bddcd7643649fd6f284ffab546
Marta Tsiruh <martatsiruch@gmail.com>
2022-01-21 01:40:13 +0100

    add task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,41 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+const galleryListContainer = document.querySelector('.gallery');
+//console.log(galleryListContainer);
+
+const galleryItemsMarkup = createElementGallery(galleryItems);
+//console.log(galleryItemsMarkup);
+
+//add string createGalleryItems in div.gallery
+galleryListContainer.insertAdjacentHTML("beforeend", galleryItemsMarkup);
+
+// Create image cards
+function createElementGallery() {
+    return galleryItems.map(({ preview, original, description }) => {
+        return `
+        <a class="gallery__item" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>
+        `
+    }).join("");
+}
+
+let gallery = new SimpleLightbox('.gallery__item', {
+    captionsData : "alt",
+    captionDelay : 250,
+});
+
+galleryListContainer.addEventListener('click', clickOnGalleryImage);
+
+function clickOnGalleryImage(event) {
+    //скасування діі браузера
+    event.preventDefault();
+
+    if(!event.target.classList.contains("gallery__image")){
+        return;
+    }
+    gallery.on('show.simplelightbox');
+}
